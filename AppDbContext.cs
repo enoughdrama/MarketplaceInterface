673bc2ec74bf26ceacca091262a7f5900a06e9a6
enoughdrama <yattaenough@gmail.com>
2025-04-02 12:52:44 +0300

    Update AppDbContext.cs

--- AppDbContext.cs
+++ AppDbContext.cs
@@ -6,8 +6,35 @@ namespace AppAuthorization
     {
         public AppDbContext() : base("DefaultConnection")
         {
+            Database.SetInitializer(new CreateDatabaseIfNotExists<AppDbContext>());
         }
 
         public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<PaymentMethod> PaymentMethods { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+        public DbSet<Commission> Commissions { get; set; }
+        public DbSet<Payout> Payouts { get; set; }
+        
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .HasIndex(u => u.Username)
+                .IsUnique();
+                
+            modelBuilder.Entity<User>()
+                .HasIndex(u => u.Email)
+                .IsUnique();
+                
+            modelBuilder.Entity<Role>()
+                .HasIndex(r => r.RoleName)
+                .IsUnique();
+                
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
