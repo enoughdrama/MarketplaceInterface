d6f383b1b645966f80a38fdb96ab5fe70d451f42
enoughdrama <yattaenough@gmail.com>
2025-04-02 12:52:09 +0300

    Create Commission.cs

---
+++ Commission.cs
@@ -0,0 +1,25 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AppAuthorization
+{
+    public class Commission
+    {
+        [Key]
+        public int CommissionId { get; set; }
+        
+        [ForeignKey("Payment")]
+        public int PaymentId { get; set; }
+        public virtual Payment Payment { get; set; }
+        
+        [Required]
+        public decimal Rate { get; set; }
+        
+        [Required]
+        public decimal Amount { get; set; }
+        
+        [Required]
+        public DateTime CalculationDate { get; set; } = DateTime.Now;
+    }
+}
