231d2e7e8380d93598ef4861441b0ec611956973
enoughdrama <yattaenough@gmail.com>
2025-04-02 12:51:17 +0300

    Create OrderItem.cs

---
+++ OrderItem.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AppAuthorization
+{
+    public class OrderItem
+    {
+        [Key]
+        public int OrderItemId { get; set; }
+        
+        [ForeignKey("Order")]
+        public int OrderId { get; set; }
+        public virtual Order Order { get; set; }
+        
+        [ForeignKey("Product")]
+        public int ProductId { get; set; }
+        public virtual Product Product { get; set; }
+        
+        [Required]
+        public int Quantity { get; set; }
+        
+        [Required]
+        public decimal Price { get; set; }
+    }
+}
