ff3900f09af5dabfe6d2b1312f9d6d27a68c9f45
enoughdrama <yattaenough@gmail.com>
2025-04-02 12:45:53 +0300

    Update User.cs

--- User.cs
+++ User.cs
@@ -1,16 +1,40 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-public class User
+namespace AppAuthorization
 {
-    [Key]
-    public int Id { get; set; }
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
 
-    [Required]
-    public string Username { get; set; }
+        [Required]
+        public string Username { get; set; }
 
-    [Required]
-    public string PasswordHash { get; set; }
+        [Required]
+        public string PasswordHash { get; set; }
 
-    [Required]
-    public string Salt { get; set; }
+        [Required]
+        public string Salt { get; set; }
+        
+        public string FirstName { get; set; }
+        
+        public string LastName { get; set; }
+        
+        public string Email { get; set; }
+        
+        public string PhoneNumber { get; set; }
+        
+        public string Address { get; set; }
+        
+        public DateTime RegistrationDate { get; set; } = DateTime.Now;
+        
+        public bool IsActive { get; set; } = true;
+        
+        [ForeignKey("Role")]
+        public int RoleId { get; set; }
+        
+        public virtual Role Role { get; set; }
+    }
 }
