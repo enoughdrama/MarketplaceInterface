1bb7f02c22fe2b36fe992cd1d550819bf483688b
enoughdrama <yattaenough@gmail.com>
2025-04-02 13:02:00 +0300

    Update User.cs

--- User.cs
+++ User.cs
@@ -10,22 +10,30 @@ namespace AppAuthorization
         public int Id { get; set; }
 
         [Required]
+        [MaxLength(50)]
         public string Username { get; set; }
 
         [Required]
+        [MaxLength(256)]
         public string PasswordHash { get; set; }
 
         [Required]
+        [MaxLength(50)]
         public string Salt { get; set; }
         
+        [MaxLength(100)]
         public string FirstName { get; set; }
         
+        [MaxLength(100)]
         public string LastName { get; set; }
         
+        [MaxLength(150)]
         public string Email { get; set; }
         
+        [MaxLength(20)]
         public string PhoneNumber { get; set; }
         
+        [MaxLength(255)]
         public string Address { get; set; }
         
         public DateTime RegistrationDate { get; set; } = DateTime.Now;
